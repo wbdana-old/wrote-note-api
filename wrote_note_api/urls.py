81f1e19ccebdb7a302ff3017d5fd455b5af7625b
William Dana <william.b.dana@gmail.com>
2018-12-25 02:12:44 -0500

    Configure Django REST Framework serializers, urls, and viewsets for Users and Groups

--- wrote_note_api/urls.py
+++ wrote_note_api/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from wrote_note_api.notes_api import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs from the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # admin
+    url(r'^', include(router.urls)),
+    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
